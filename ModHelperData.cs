7816951a0e8c680fda0dce3d9e473c6dc8ef9fde
ixenderous <ixenderous@gmail.com>
2025-05-09 20:09:01 +1200

    Removed warning

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,10 +3,10 @@ namespace TempleBug;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "47.0";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Name = "TempleBug";
 
-    public const string Description = "Reintroduces the temple bug that allowed for extra cash generation in CHIMPS mode.\n\nWarning: This currently doesnt function exactly like the original temple bug. The original temple bug could stack with multiple temples while this only ports the vanilla support temple extra cash behaviour which does not stack (I think) to CHIMPS. I will re-add the bug as it was once I can research how it actually functioned, although this is good enough for now.";
+    public const string Description = "Reintroduces the temple bug that allowed for extra cash generation in CHIMPS mode.";
 
     public const string RepoOwner = "iXendeRouS";
     public const string RepoName = "TempleBug";
